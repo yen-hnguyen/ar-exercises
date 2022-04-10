cba6995dec834fcf898144196aef99346bd712cb
Yen Nguyen <yen.hnguyen17@gmail.com>
2022-04-09 23:25:18 -0400

    exercise 3 - delete the third store

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -2,7 +2,11 @@ require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
 
-puts "Exercise 3"
-puts "----------"
+puts 'Exercise 3'
+puts '----------'
 
 # Your code goes here ...
+@store3 = Store.find_by(id: 3)
+@store3.destroy
+
+puts Store.count
