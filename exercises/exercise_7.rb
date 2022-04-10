22c3ecd9a7f3e258d267f78ac005e5c74cff81db
Yen Nguyen <yen.hnguyen17@gmail.com>
2022-04-10 00:36:21 -0400

    exercise 7 - validations for both model

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -6,7 +6,12 @@ require_relative './exercise_4'
 require_relative './exercise_5'
 require_relative './exercise_6'
 
-puts "Exercise 7"
-puts "----------"
+puts 'Exercise 7'
+puts '----------'
 
 # Your code goes here ...
+puts 'What do you want to name the store?'
+store_name = $stdin.gets.chomp
+
+new_store = Store.create(name: store_name)
+puts new_store.errors.full_messages
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,10 @@
 class Store < ActiveRecord::Base
   has_many :employees
+
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue,
+            numericality: {
+              only_interger: true,
+              greater_than_or_equal_to: 0,
+            }
 end
