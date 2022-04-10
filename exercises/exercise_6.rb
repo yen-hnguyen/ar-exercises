ea4a53392e227e975ef1cb17307c5e9dd67d73ac
Yen Nguyen <yen.hnguyen17@gmail.com>
2022-04-10 00:02:53 -0400

    exercise 6 - one-to-many association

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -5,7 +5,34 @@ require_relative './exercise_3'
 require_relative './exercise_4'
 require_relative './exercise_5'
 
-puts "Exercise 6"
-puts "----------"
+puts 'Exercise 6'
+puts '----------'
 
 # Your code goes here ...
+
+@store1.employees.create(
+  first_name: 'Saphira',
+  last_name: 'Sullilatu',
+  hourly_rate: 60,
+)
+@store1.employees.create(
+  first_name: 'Chistine',
+  last_name: 'Thompson',
+  hourly_rate: 90,
+)
+@store1.employees.create(
+  first_name: 'Jackie',
+  last_name: 'Trac',
+  hourly_rate: 80,
+)
+
+@store2.employees.create(
+  first_name: 'Michelle',
+  last_name: 'Le',
+  hourly_rate: 70,
+)
+@store2.employees.create(
+  first_name: 'Yen',
+  last_name: 'Nguyen',
+  hourly_rate: 100,
+)
