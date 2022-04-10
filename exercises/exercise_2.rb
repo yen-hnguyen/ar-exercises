4b38822d060344add3e051273e16181e2e5b6a2e
Yen Nguyen <yen.hnguyen17@gmail.com>
2022-04-09 23:21:56 -0400

    exercise 2 - update the first store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -1,7 +1,12 @@
 require_relative '../setup'
 require_relative './exercise_1'
 
-puts "Exercise 2"
-puts "----------"
+puts 'Exercise 2'
+puts '----------'
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store2 = Store.find_by(id: 2)
+@store1.update(name: 'Victoria')
+
+pp @store1
