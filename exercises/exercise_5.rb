b32fc728bc21569954db176a7d9519a4cbfe0c6e
Yen Nguyen <yen.hnguyen17@gmail.com>
2022-04-09 23:57:03 -0400

    exercise 5 - calculations

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -4,7 +4,15 @@ require_relative './exercise_2'
 require_relative './exercise_3'
 require_relative './exercise_4'
 
-puts "Exercise 5"
-puts "----------"
+puts 'Exercise 5'
+puts '----------'
 
 # Your code goes here ...
+@total_annual_rev = Store.sum(:annual_revenue)
+puts @total_annual_rev
+
+@avg_annual_rev = Store.average(:annual_revenue)
+puts @avg_annual_rev
+
+@over_1m_rev_store_count = Store.where('annual_revenue >= ?', 1_000_000).count
+puts @over_1m_rev_store_count
